937443458cb4199e6c5514ef97357ddc6ebb6252
Dementedduck <155681526+Dementedduck@users.noreply.github.com>
2024-09-12 16:51:33 +0100

    Add entire script Add the method

--- Cipher.rb
+++ Cipher.rb
@@ -1,3 +1,10 @@
-def caesar_cipher(string, num)
-  
+
+
+def caesar_cipher(string, input = 0)
+  alphabet = [*'a'..'z', *'A'..'Z']
+  rotator = Hash[alphabet.zip(alphabet.rotate(input))]
+  result = string.chars.map { |char| rotator.fetch(char, " ") }
+  result.join("")
 end
+
+puts caesar_cipher("hEllo", 2)
